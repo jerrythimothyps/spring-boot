433e19a4845d31286ea957cc71f303d5f005c4bb
Jerry Thimothy <jerry.thimothy@publicissapient.com>
2021-02-28 15:28:25 +0400

    dummy get api

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -9,6 +12,18 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@RequestMapping("/api/todos")
+	public String getTodos() {
+		try {
+			return Unirest.get("http://www.mocky.io/v2/5a9ce37b3100004f00ab5154").asString().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
 	}
 
 }
